1af30e7cf4b0302851fc178ec340a243e93d669a
GrayJack <gr41.j4ck@gmail.com>
2020-06-12 04:18:11 -0300

    run_bytes -> run

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -3,7 +3,7 @@ use janetrs::client::{Error, JanetClient};
 fn main() -> Result<(), Error> {
     let client = JanetClient::init()?.with_default_env();
 
-    client.run_bytes(b"(print `Hello from Janet!`)")?;
+    client.run("(print `Hello from Janet!`)")?;
 
     Ok(())
 }
