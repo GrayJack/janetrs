4734b01905857711d26b947a012b274fd3883338
GrayJack <gr41.j4ck@gmail.com>
2020-06-16 00:39:24 -0300

    Update janet_ll and add top level crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! # Janetrs
+//!
+//! A crate with high level bindings to Janet C API.
+//!
+//! TODO: Explain crate features
+//!
+//! ## TODO: Lib level
+//!  - We still don't know exactly how Janet panics would work on Rust, so we need to
+//!    explore that and documment it
+//!  - Janet requires allocations being possible, how do we enforce `alloc` on `no_std`
+//!    environment?
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub use janet_ll as janet_sys;
