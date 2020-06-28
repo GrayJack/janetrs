fbaca1bf5242cb3a66eb5b6058d81c8aa9234e09
GrayJack <gr41.j4ck@gmail.com>
2020-06-27 22:22:11 -0300

    Introduce JanetSymbol and JanetKeyword

--- src/types.rs
+++ src/types.rs
@@ -3,13 +3,13 @@ use core::{cmp::Ordering, fmt};
 
 use janet_ll::{
     janet_type, janet_wrap_array, janet_wrap_boolean, janet_wrap_buffer, janet_wrap_fiber,
-    janet_wrap_integer, janet_wrap_nil, janet_wrap_number, janet_wrap_string, janet_wrap_struct,
-    janet_wrap_table, janet_wrap_tuple, Janet as CJanet, JanetType as CJanetType,
-    JanetType_JANET_ABSTRACT, JanetType_JANET_ARRAY, JanetType_JANET_BOOLEAN,
-    JanetType_JANET_BUFFER, JanetType_JANET_CFUNCTION, JanetType_JANET_FIBER,
-    JanetType_JANET_FUNCTION, JanetType_JANET_KEYWORD, JanetType_JANET_NIL, JanetType_JANET_NUMBER,
-    JanetType_JANET_POINTER, JanetType_JANET_STRING, JanetType_JANET_STRUCT,
-    JanetType_JANET_SYMBOL, JanetType_JANET_TABLE, JanetType_JANET_TUPLE,
+    janet_wrap_integer, janet_wrap_keyword, janet_wrap_nil, janet_wrap_number, janet_wrap_string,
+    janet_wrap_struct, janet_wrap_symbol, janet_wrap_table, janet_wrap_tuple, Janet as CJanet,
+    JanetType as CJanetType, JanetType_JANET_ABSTRACT, JanetType_JANET_ARRAY,
+    JanetType_JANET_BOOLEAN, JanetType_JANET_BUFFER, JanetType_JANET_CFUNCTION,
+    JanetType_JANET_FIBER, JanetType_JANET_FUNCTION, JanetType_JANET_KEYWORD, JanetType_JANET_NIL,
+    JanetType_JANET_NUMBER, JanetType_JANET_POINTER, JanetType_JANET_STRING,
+    JanetType_JANET_STRUCT, JanetType_JANET_SYMBOL, JanetType_JANET_TABLE, JanetType_JANET_TUPLE,
 };
 
 pub mod array;
@@ -17,6 +17,7 @@ pub mod buffer;
 pub mod fiber;
 pub mod string;
 pub mod structs;
+pub mod symbol;
 pub mod table;
 pub mod tuple;
 
@@ -25,6 +26,7 @@ pub use buffer::JanetBuffer;
 pub use fiber::JanetFiber;
 pub use string::JanetString;
 pub use structs::JanetStruct;
+pub use symbol::{JanetKeyword, JanetSymbol};
 pub use table::JanetTable;
 pub use tuple::JanetTuple;
 
@@ -94,6 +96,7 @@ impl Janet {
         }
     }
 
+    /// Create a fiber [`Janet`] with `value`.
     #[inline]
     pub fn fiber(value: JanetFiber<'_>) -> Self {
         Self {
@@ -101,6 +104,7 @@ impl Janet {
         }
     }
 
+    /// Create a tuple [`Janet`] with `value`.
     #[inline]
     pub fn tuple(value: JanetTuple<'_>) -> Self {
         Self {
@@ -108,6 +112,7 @@ impl Janet {
         }
     }
 
+    /// Create a string [`Janet`] with `value`.
     #[inline]
     pub fn string(value: JanetString<'_>) -> Self {
         Self {
@@ -115,10 +120,27 @@ impl Janet {
         }
     }
 
+    /// Create a struct [`Janet`] with `value`.
     #[inline]
     pub fn structs(value: JanetStruct<'_>) -> Self {
         Self {
             inner: unsafe { janet_wrap_struct(value.raw) },
+        }
+    }
+
+    /// Create a symbol [`Janet`] with `value`.
+    #[inline]
+    pub fn symbol(value: JanetSymbol<'_>) -> Self {
+        Self {
+            inner: unsafe { janet_wrap_symbol(value.raw) },
+        }
+    }
+
+    /// Create a keyword [`Janet`] with `value`.
+    #[inline]
+    pub fn keyword(value: JanetKeyword<'_>) -> Self {
+        Self {
+            inner: unsafe { janet_wrap_keyword(value.raw) },
         }
     }
 
@@ -132,20 +154,18 @@ impl Janet {
 }
 
 impl fmt::Debug for Janet {
-    /// janet_ll::Janet is a C enum, so we cannot display much of a useful for debugging
-    /// data with the default derive Debug, so we add phantom debug data to help debugging
-    /// Janet values, therefore, all data shown with `phantom` prefix it's just for debug
-    /// purposes and doesn't exist on actual Janet struct.
+    #[inline]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Janet")
             .field("inner", &self.inner)
-            .field("phantom_kind", &self.kind())
-            .field("phantom_value", &format_args!("{}", self))
+            .field("kind", &self.kind())
+            .field("value", &format_args!("{}", self))
             .finish()
     }
 }
 
 impl fmt::Display for Janet {
+    #[inline]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         // There some overhead for doing this dance, but the only way to get the Janet value from
         // C API and transform into &str to display it.
@@ -221,6 +241,14 @@ impl From<JanetString<'_>> for Janet {
 
 impl From<JanetStruct<'_>> for Janet {
     fn from(val: JanetStruct<'_>) -> Self { Self::structs(val) }
+}
+
+impl From<JanetSymbol<'_>> for Janet {
+    fn from(val: JanetSymbol<'_>) -> Self { Self::symbol(val) }
+}
+
+impl From<JanetKeyword<'_>> for Janet {
+    fn from(val: JanetKeyword<'_>) -> Self { Self::keyword(val) }
 }
 
 impl From<Janet> for CJanet {
---
+++ src/types/symbol.rs
@@ -0,0 +1,75 @@
+//! Module dealing with symbols and keywords
+use core::marker::PhantomData;
+
+use janet_ll::janet_symbol;
+
+
+#[derive(Debug)]
+pub struct JanetSymbol<'data> {
+    pub(crate) raw: *const u8,
+    phantom: PhantomData<&'data ()>,
+}
+
+impl JanetSymbol<'_> {
+    /// Create a [`JanetSymbol`] with given `name`.
+    ///
+    /// If the given `name` is bigger than i32::MAX the generated symbol will have a name
+    /// trucated to that max size. That's unrealistic thought.
+    #[inline]
+    pub fn new(name: impl AsRef<[u8]>) -> Self {
+        let val = name.as_ref();
+
+        let len = if val.len() > i32::MAX as usize {
+            i32::MAX
+        } else {
+            val.len() as i32
+        };
+
+        Self {
+            raw:     unsafe { janet_symbol(val.as_ptr(), len) },
+            phantom: PhantomData,
+        }
+    }
+
+    /// Return a raw pointer to the symbol raw structure.
+    ///
+    /// The caller must ensure that the buffer outlives the pointer this function returns,
+    /// or else it will end up pointing to garbage.
+    #[inline]
+    pub fn as_raw(&self) -> *const u8 { self.raw }
+}
+
+#[derive(Debug)]
+pub struct JanetKeyword<'data> {
+    pub(crate) raw: *const u8,
+    phantom: PhantomData<&'data ()>,
+}
+
+impl JanetKeyword<'_> {
+    /// Create a [`JanetKeyword`] with given `name`.
+    ///
+    /// If the given `name` is bigger than i32::MAX the generated symbol will have a name
+    /// trucated to that max size. That's unrealistic thought.
+    #[inline]
+    pub fn new(name: impl AsRef<[u8]>) -> Self {
+        let val = name.as_ref();
+
+        let len = if val.len() > i32::MAX as usize {
+            i32::MAX
+        } else {
+            val.len() as i32
+        };
+
+        Self {
+            raw:     unsafe { janet_symbol(val.as_ptr(), len) },
+            phantom: PhantomData,
+        }
+    }
+
+    /// Return a raw pointer to the keyword raw structure.
+    ///
+    /// The caller must ensure that the buffer outlives the pointer this function returns,
+    /// or else it will end up pointing to garbage.
+    #[inline]
+    pub fn as_raw(&self) -> *const u8 { self.raw }
+}
