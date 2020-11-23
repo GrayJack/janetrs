3adb70ac4b7499c187a195fab7fbcc50f3e3c2f9
GrayJack <gr41.j4ck@gmail.com>
2020-11-23 01:21:43 -0300

    Improve Janet docs

--- src/types.rs
+++ src/types.rs
@@ -84,6 +84,36 @@ impl Display for JanetConversionError {
 /// ```
 ///
 /// ## Extracting/Unwraping Janet values
+/// To extract/unwrap the `Janet` value you can use the [`Janet::unwrap`] method, that
+/// will return a [`TaggedJanet`] that you can pattern match to use the extracted value.
+///
+/// ### Example
+/// ```
+/// use janetrs::types::{Janet, TaggetJanet};
+/// # let _client = janetrs::client::JanetClient::init().unwrap();
+///
+/// let jnt = Janet::wrap(10); // A Number Janet
+///
+/// match jnt.unwrap() {
+///     TaggedJanet::Abstract(abstract) => {}
+///     TaggedJanet::Array(array) => {}
+///     TaggedJanet::Boolean(boolean) => {}
+///     TaggedJanet::Buffer(buffer) => {}
+///     TaggedJanet::CFunction(c_fun) => {}
+///     TaggedJanet::Fiber(fiber) => {}
+///     TaggedJanet::Function(func) => {}
+///     TaggedJanet::Keyword(keyword) => {}
+///     TaggedJanet::Nil => {}
+///     TaggedJanet::Number(num) => {}
+///     TaggedJanet::Pointer(ptr) => {}
+///     TaggedJanet::String(string) => {}
+///     TaggedJanet::Struct(st) => {}
+///     TaggedJanet::Symbol(symbol) => {}
+///     TaggedJanet::Table(table) => {}
+///     TaggedJanet::Tuple(tuple) => {}
+/// };
+/// ```
+///
 /// To extract/unwrap the `Janet` value you can use the [`Janet::try_unwrap`] method, that
 /// will return a [`Result`] either with the resulted type of and conversion error.
 ///
